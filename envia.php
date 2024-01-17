9472047a1db0c42c966812701e1232f1435ebdba
Lilia270 <liliarosaestudos@gmail.com>
2024-01-17 09:09:54 -0300

    atualizacao html e php

--- envia.php
+++ envia.php
@@ -3,11 +3,12 @@
     $nome = addcslashes($_POST['nome']);
     $email = addcslashes($_POST['email']);
     $telefone = addcslashes($_POST['telefone']);
+    $mensagem = addslashes($_POST['mensagem']);
 
     $para = "liliaestudos@gmail.com";
     $assunto = "Emprego - site Lilia Rosa";
 
-    $corpo = "Nome:".$nome."\n"."E-mail:". $email."\n". "Telefone". $telefone;
+    $corpo = "Nome:".$nome."\n"."E-mail:". $email."\n". "Telefone". $telefone. "\n". "Mensagem" .$mensagem;
     
     $cabeca ="From: liliarosacoelho@gmail.com" . "\n"."Reply-to".$email."\n"."X=Mailer:PHP/".phpversion();
 
