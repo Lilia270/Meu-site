42fdc17378cbebe01e8b16ab45f17303be57ad56
Lilia270 <liliarosaestudos@gmail.com>
2024-01-17 11:04:26 -0300

    nova versão php

--- envia.php
+++ envia.php
@@ -1,34 +1,28 @@
 <?php
+// Passando os dados obtidos pelo formulário para as variáveis abaixo
+$nomeremetente = $_POST['nome'];
+$emaildestinatario = 'lilirosaaestudos@gmail.com';// Digite seu e-mail aqui, lembrando que o e-mail deve estar em seu servidor web
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$mensagem = $_POST['mensagem'];
 
-$nome = '';
-$email = '';
-$telefone = '';
-$mensagem = '';
+/* Montando a mensagem a ser enviada no corpo do e-mail. */
+$mensagemHTML = '
+Formulario de Contato
+Nome completo e idade:'.$nomeremetente.' 
+Email: '.$email.' 
+Telefone: '.$telefone.' 
+Mensagem: '.$mensagem.'';
 
-    if(isset($_POST['email']) && !empty($_POST['email'])) {
-
-
-    $nome = addslashes($_POST['nome']);
-    $email = addslashes($_POST['email']);
-    $telefone = addslashes($_POST['telefone']);
-    $mensagem = addslashes($_POST['mensagem']);
-
-    }
-
-    $para = "lilirosaaestudos@gmail.com";
-    $assunto = "Emprego - site Lilia Rosa";
-
-    $corpo = "Nome:".$nome."\r\n".
-             "E-mail:".$email."\r\n" .
-             "Telefone:". $telefone. "\r\n".
-             "Mensagem" .$mensagem;
-    
-    $cabeca ="From: liliarosacoelho@gmail.com" . "\r\n"."Reply-to".$email."\n"."X=Mailer:PHP/".phpversion();
-
-    if(mail($para,$assunto,$corpo, $cabeca)){
-        echo("E-mail enviado!");
-
-    }else{
-        echo("Houve um erro ao enviar o email");
-    }
+// O remetente deve ser um e-mail do seu domínio conforme determina a RFC 822.
+// O return-path deve ser ser o mesmo e-mail do remetente.
+$headers = "MIME-Version: 1.1\r\n";
+$headers .= "Content-type: text/html; charset=utf-8\r\n";
+$headers .= "From: $nomeremetente\r\n";
+// remetente
+$headers .= "Return-Path: $emaildestinatario \r\n";
+// return-path
+$envio = mail($emaildestinatario, $nomeremetente, $mensagemHTML, $nomeremetente);
+if($envio)
+echo "<script>location.href='https://youtube.com'</script>";// Página que será redirecionada
 ?>
