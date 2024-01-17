16eec2f600db4921b03d233a6739ec8e66f12f4d
Lilia270 <liliarosaestudos@gmail.com>
2024-01-17 09:39:04 -0300

    PHP nova versao

--- envia.php
+++ envia.php
@@ -1,16 +1,25 @@
 <?php
 
-    $nome = addcslashes($_POST['nome']);
-    $email = addcslashes($_POST['email']);
-    $telefone = addcslashes($_POST['telefone']);
+
+    if(isset($_POST['email']) && !empty($_POST['email'])) {
+
+
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
     $mensagem = addslashes($_POST['mensagem']);
 
-    $para = "liliaestudos@gmail.com";
+    }
+
+    $para = "lilirosaaestudos@gmail.com";
     $assunto = "Emprego - site Lilia Rosa";
 
-    $corpo = "Nome:".$nome."\n"."E-mail:". $email."\n". "Telefone". $telefone. "\n". "Mensagem" .$mensagem;
+    $corpo = "Nome:".$nome."\r\n".
+             "E-mail:".$email."\r\n" .
+             "Telefone:". $telefone. "\r\n".
+             "Mensagem" .$mensagem;
     
-    $cabeca ="From: liliarosacoelho@gmail.com" . "\n"."Reply-to".$email."\n"."X=Mailer:PHP/".phpversion();
+    $cabeca ="From: liliarosacoelho@gmail.com" . "\r\n"."Reply-to".$email."\n"."X=Mailer:PHP/".phpversion();
 
     if(mail($para,$assunto,$corpo, $cabeca)){
         echo("E-mail enviado!");
